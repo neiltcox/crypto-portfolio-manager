a8a2eb90b8d310226176ea6d4507e644f2404062
Jakson Kallio <jaksonkallio@gmail.com>
2022-06-15 19:27:54 -0500

    Fetching market data and assets

--- src/service/marketdata.go
+++ src/service/marketdata.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -11,27 +12,79 @@ import (
 )
 
 type Asset struct {
-	Ticker    string
+	Symbol    string
 	MarketCap uint64
+	Volume    uint64
+
+	// An approximate price used for informational purposes only.
+	// Do NOT use for creating Schedules.
+	// Could also be used to sanity-check exchange prices of assets.
+	ApproxPrice float64
+}
+
+type ListingsResponse struct {
+	Listings []Listing `json:"data"`
+}
+
+type Listing struct {
+	Name   string `json:"name"`
+	Symbol string `json:"symbol"`
+	Quotes Quotes `json:"quote"`
+}
+
+type Quotes struct {
+	USD Quote `json:"USD"`
+}
+
+type Quote struct {
+	Price     float64 `json:"price"`
+	Volume    float64 `json:"volume_24h"`
+	MarketCap float64 `json:"market_cap"`
 }
 
 var assets map[string]*Asset = make(map[string]*Asset)
 
 // Refreshes all market data.
 func RefreshMarketData(cfg config.Config) {
-	resp, err := marketDataApi(cfg, "cryptocurrency/listings/latest", url.Values{
+	responseBytes, err := marketDataApi(cfg, "cryptocurrency/listings/latest", url.Values{
 		"start":   []string{"1"},
-		"limit":   []string{"5000"},
+		"limit":   []string{"500"},
 		"convert": []string{"USD"},
 	})
 	if err != nil {
 		log.Printf("Could not reach market data API: %s", err)
 	}
 
-	log.Println(string(resp))
+	listingsResponse := ListingsResponse{}
+	json.Unmarshal(responseBytes, &listingsResponse)
+
+	for _, listing := range listingsResponse.Listings {
+		asset, exists := assets[listing.Symbol]
+		if !exists {
+			asset = &Asset{}
+			assets[listing.Symbol] = asset
+		}
+
+		asset.Symbol = listing.Symbol
+		asset.MarketCap = uint64(listing.Quotes.USD.MarketCap)
+		asset.Volume = uint64(listing.Quotes.USD.Volume)
+		asset.ApproxPrice = listing.Quotes.USD.Price
+	}
+
+	for _, asset := range assets {
+		log.Printf("symbol: %s", asset.Symbol)
+	}
 }
 
 func marketDataApi(cfg config.Config, endpoint string, query url.Values) ([]byte, error) {
+	if len(cfg.MarketData.ApiKey) == 0 {
+		return nil, fmt.Errorf("did not configure a market data API key")
+	}
+
+	if len(cfg.MarketData.BaseUrl) == 0 {
+		return nil, fmt.Errorf("did not configure a market data API base URL")
+	}
+
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", fmt.Sprintf("%s%s", cfg.MarketData.BaseUrl, endpoint), nil)
 	if err != nil {
